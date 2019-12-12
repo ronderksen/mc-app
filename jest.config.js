ce980126097bc4f1b9a1dee94b8cb7a402049bf7
Ron Derksen <rderksen@wehkamp.nl>
2019-12-12 15:46:30 +0100

    feat: initial commit

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+const {defaults} = require('jest-config');
+
+module.exports = {
+  roots: [
+    '<rootDir>/pages',
+    '<rootDir>/lib'
+  ],
+  moduleNameMapper: {
+    "^.+\\.css$": "identity-obj-proxy",
+  },
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  setupFilesAfterEnv: [
+    './jest-setup.ts'
+  ],
+  preset: "ts-jest",
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        jsx: 'react'
+      }
+    }
+  }
+};
